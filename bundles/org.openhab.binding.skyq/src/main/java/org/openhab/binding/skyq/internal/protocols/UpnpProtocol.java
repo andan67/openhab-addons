c5e8f8e04f65ed9c5223773e42677844ec7b5942
andan67 <andandev67@gmail.com>
2021-12-05 17:38:26 +0100

    Implemented request to request current media and according channel.

---
+++ bundles/org.openhab.binding.skyq/src/main/java/org/openhab/binding/skyq/internal/protocols/UpnpProtocol.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.skyq.internal.protocols;
+
+import java.text.MessageFormat;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.util.StringContentProvider;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.http.HttpStatus;
+import org.openhab.binding.skyq.internal.SkyQHandlerFactory;
+import org.openhab.binding.skyq.internal.models.MediaInfo;
+import org.openhab.binding.skyq.internal.models.ServiceDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
+/**
+ * @author andan - Initial contribution
+ */
+public class UpnpProtocol {
+
+    private final Logger logger = LoggerFactory.getLogger(UpnpProtocol.class);
+
+    static final int PROTOCOL_TIMEOUT = 1000;
+
+    static final String SOAP_CONTROL_BASE_URL = "http://{0}:49153{1}";
+    static final String SOAP_DESCRIPTION_BASE_URL = "http://{0}:49153/description{1}.xml";
+    static final String SKY_PLAY_URN = "urn:nds-com:serviceId:SkyPlay";
+    static final String SOAP_ACTION = "\"urn:schemas-nds-com:service:SkyPlay:2#{0}\"";
+    static final String SOAP_PAYLOAD = "<s:Envelope xmlns:s=''http://schemas.xmlsoap.org/soap/envelope/'' s:encodingStyle=''http://schemas.xmlsoap.org/soap/encoding/''>\n"
+            + "   <s:Body>\n" + "       <u:{0} xmlns:u=\"urn:schemas-nds-com:service:SkyPlay:2\">\n"
+            + "           <InstanceID>0</InstanceID>\n" + "       </u:{0}>\n" + "   </s:Body>\n" + "</s:Envelope>\n";
+
+    static final String SOAP_RESPONSE = "u:{0}Response";
+    static final String SOAP_USER_AGENT = "SKYPLUS_skyplus";
+    static final String UPNP_GET_MEDIA_INFO = "GetMediaInfo";
+    static final String UPNP_GET_TRANSPORT_INFO = "GetTransportInfo";
+
+    private final String host;
+    protected String baseUrl;
+    private final HttpClient httpClient;
+    private ServiceDescription.Service playService;
+    private XStream xStream;
+
+    public UpnpProtocol(String host, HttpClient httpClient) {
+        this.host = host;
+        this.httpClient = httpClient;
+
+        xStream = new XStream(new StaxDriver());
+        xStream.allowTypesByWildcard(new String[] { SkyQHandlerFactory.class.getPackageName() + ".**" });
+        xStream.setClassLoader(getClass().getClassLoader());
+        xStream.ignoreUnknownElements();
+        xStream.processAnnotations(new Class[] { ServiceDescription.class, MediaInfo.class });
+
+        playService = findPlayService();
+    }
+
+    private ServiceDescription.Service findPlayService() {
+        for (int id = 0; id < 50; id++) {
+            String url = MessageFormat.format(SOAP_DESCRIPTION_BASE_URL, host, Integer.toString(id));
+            Request req = httpClient.newRequest(url);
+            req.agent(SOAP_USER_AGENT);
+            req.method(HttpMethod.GET);
+            try {
+                ContentResponse res = req.send();
+                if (res.getStatus() == HttpStatus.OK_200) {
+                    ServiceDescription serviceDescription = (ServiceDescription) xStream
+                            .fromXML(res.getContentAsString());
+                    for (ServiceDescription.Service service : serviceDescription.device.serviceList) {
+                        if (SKY_PLAY_URN.equals(service.serviceId)) {
+                            return service;
+                        }
+                    }
+                }
+            } catch (InterruptedException | TimeoutException | ExecutionException ex) {
+                break;
+            }
+        }
+        return null;
+    }
+
+    public MediaInfo requestMediaInfo() {
+        String url = MessageFormat.format(SOAP_CONTROL_BASE_URL, host, playService.controlURL);
+        String soapAction = MessageFormat.format(SOAP_ACTION, UPNP_GET_MEDIA_INFO);
+        String soapPayload = MessageFormat.format(SOAP_PAYLOAD, UPNP_GET_MEDIA_INFO);
+        Request req = httpClient.newRequest(url);
+        req.agent(SOAP_USER_AGENT);
+        req.method(HttpMethod.POST);
+        req.header("SOAPACTION", soapAction);
+        req.header(HttpHeader.CONTENT_TYPE, "text/xml");
+        req.content(new StringContentProvider(soapPayload));
+        try {
+            ContentResponse res = req.send();
+            if (res.getStatus() == HttpStatus.OK_200) {
+                MediaInfo mediaInfo = (MediaInfo) xStream.fromXML(res.getContentAsString());
+                return mediaInfo;
+            }
+        } catch (InterruptedException | TimeoutException | ExecutionException ex) {
+            return null;
+        }
+        return null;
+    }
+}
--- bundles/org.openhab.binding.skyq/src/test/java/org/openhab/binding/skyq/internal/Tests.java
+++ bundles/org.openhab.binding.skyq/src/test/java/org/openhab/binding/skyq/internal/Tests.java
@@ -1,20 +1,18 @@
 package org.openhab.binding.skyq.internal;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-import com.thoughtworks.xstream.annotations.XStreamImplicit;
-import org.eclipse.jdt.annotation.Nullable;
-//import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
+import java.text.MessageFormat;
+
+import javax.xml.xpath.*;
+
+import org.junit.jupiter.api.Test;
+import org.openhab.binding.skyq.internal.models.MediaInfo;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 public class Tests {
-
 
     @Test
     public void testXML2Description() {
@@ -23,100 +21,72 @@ public class Tests {
         xStream.allowTypesByWildcard(new String[] { SkyQHandlerFactory.class.getPackageName() + ".**" });
         xStream.setClassLoader(getClass().getClassLoader());
         xStream.ignoreUnknownElements();
-        xStream.processAnnotations(Tests.class.getClasses());
-        String xml =
-                "<?xml version=\"1.0\"?>\n" +
-                "<root xmlns=\"urn:schemas-upnp-org:device-1-0\">\n" +
-                "    <specVersion>\n" +
-                "        <major>1</major>\n" +
-                "        <minor>0</minor>\n" +
-                "    </specVersion>\n" +
-                "    <device>\n" +
-                "        <deviceType>urn:schemas-nds-com:device:GatewaySkyControl:2</deviceType>\n" +
-                "        <modelDescription>6763A3</modelDescription>\n" +
-                "        <modelName>Sky+HD</modelName>\n" +
-                "        <modelNumber>Q180.000.16.01L (5f5ezi9)</modelNumber>\n" +
-                "        <friendlyName>BSKYB_GATEWAY</friendlyName>\n" +
-                "        <manufacturer>HUMAX</manufacturer>\n" +
-                "        <nds:X_NDS_ChipID xmlns:nds=\"urn:schemas-nds-com:device-1-0\">2631057532</nds:X_NDS_ChipID>\n" +
-                "        <nds:X_NDS_Configuration_Manifest xmlns:nds=\"urn:schemas-nds-com:device-1-0\">http://192.168.178.23:0//</nds:X_NDS_Configuration_Manifest>\n" +
-                "        <nds:X_NDS_PortNumber xmlns:nds=\"urn:schemas-nds-com:device-1-0\">1234</nds:X_NDS_PortNumber>\n" +
-                "        <nds:X_NDS_DSSURI xmlns:nds=\"urn:schemas-nds-com:device-1-0\">http://192.168.178.23:8080</nds:X_NDS_DSSURI>\n" +
-                "        <nds:X_NDS_LowPowerCapability xmlns:nds=\"urn:schemas-nds-com:device-1-0\">false</nds:X_NDS_LowPowerCapability>\n" +
-                "        <nds:X_NDS_StreamingCapable xmlns:nds=\"urn:schemas-nds-com:device-1-0\">false</nds:X_NDS_StreamingCapable>\n" +
-                "        <UDN>uuid:444D5276-3247-4761-7465-3438b7405094</UDN>\n" +
-                "        <serviceList>\n" +
-                "            <service>\n" +
-                "                <serviceType>urn:schemas-nds-com:service:SkyPlay:2</serviceType>\n" +
-                "                <serviceId>urn:nds-com:serviceId:SkyPlay</serviceId>\n" +
-                "                <SCPDURL>/player_avt.xml</SCPDURL>\n" +
-                "                <controlURL>/444D5276-3247-4761-7465-3438b7405094SkyPlay</controlURL>\n" +
-                "                <eventSubURL>/444D5276-3247-4761-7465-3438b7405094SkyPlay</eventSubURL>\n" +
-                "            </service>\n" +
-                "            <service>\n" +
-                "                <serviceType>urn:schemas-nds-com:service:SkyCM:2</serviceType>\n" +
-                "                <serviceId>urn:nds-com:serviceId:SkyCM</serviceId>\n" +
-                "                <SCPDURL>/player_cm.xml</SCPDURL>\n" +
-                "                <controlURL>/444D5276-3247-4761-7465-3438b7405094SkyCM</controlURL>\n" +
-                "                <eventSubURL>/444D5276-3247-4761-7465-3438b7405094SkyCM</eventSubURL>\n" +
-                "            </service>\n" +
-                "            <service>\n" +
-                "                <serviceType>urn:schemas-nds-com:service:SkyRC:2</serviceType>\n" +
-                "                <serviceId>urn:nds-com:serviceId:SkyRC</serviceId>\n" +
-                "                <SCPDURL>/player_rc.xml</SCPDURL>\n" +
-                "                <controlURL>/444D5276-3247-4761-7465-3438b7405094SkyRC</controlURL>\n" +
-                "                <eventSubURL>/444D5276-3247-4761-7465-3438b7405094SkyRC</eventSubURL>\n" +
-                "            </service>\n" +
-                "        </serviceList>\n" +
-                "    </device>\n" +
-                "    <URLBase>http://192.168.178.23:49153/</URLBase>\n" +
-                "</root>";
+        xStream.processAnnotations(org.openhab.binding.skyq.internal.models.ServiceDescription.class);
+        String xml = "<?xml version=\"1.0\"?>\n" + "<root xmlns=\"urn:schemas-upnp-org:device-1-0\">\n"
+                + "    <specVersion>\n" + "        <major>1</major>\n" + "        <minor>0</minor>\n"
+                + "    </specVersion>\n" + "    <device>\n"
+                + "        <deviceType>urn:schemas-nds-com:device:GatewaySkyControl:2</deviceType>\n"
+                + "        <modelDescription>6763A3</modelDescription>\n" + "        <modelName>Sky+HD</modelName>\n"
+                + "        <modelNumber>Q180.000.16.01L (5f5ezi9)</modelNumber>\n"
+                + "        <friendlyName>BSKYB_GATEWAY</friendlyName>\n"
+                + "        <manufacturer>HUMAX</manufacturer>\n"
+                + "        <nds:X_NDS_ChipID xmlns:nds=\"urn:schemas-nds-com:device-1-0\">2631057532</nds:X_NDS_ChipID>\n"
+                + "        <nds:X_NDS_Configuration_Manifest xmlns:nds=\"urn:schemas-nds-com:device-1-0\">http://192.168.178.23:0//</nds:X_NDS_Configuration_Manifest>\n"
+                + "        <nds:X_NDS_PortNumber xmlns:nds=\"urn:schemas-nds-com:device-1-0\">1234</nds:X_NDS_PortNumber>\n"
+                + "        <nds:X_NDS_DSSURI xmlns:nds=\"urn:schemas-nds-com:device-1-0\">http://192.168.178.23:8080</nds:X_NDS_DSSURI>\n"
+                + "        <nds:X_NDS_LowPowerCapability xmlns:nds=\"urn:schemas-nds-com:device-1-0\">false</nds:X_NDS_LowPowerCapability>\n"
+                + "        <nds:X_NDS_StreamingCapable xmlns:nds=\"urn:schemas-nds-com:device-1-0\">false</nds:X_NDS_StreamingCapable>\n"
+                + "        <UDN>uuid:444D5276-3247-4761-7465-3438b7405094</UDN>\n" + "        <serviceList>\n"
+                + "            <service>\n"
+                + "                <serviceType>urn:schemas-nds-com:service:SkyPlay:2</serviceType>\n"
+                + "                <serviceId>urn:nds-com:serviceId:SkyPlay</serviceId>\n"
+                + "                <SCPDURL>/player_avt.xml</SCPDURL>\n"
+                + "                <controlURL>/444D5276-3247-4761-7465-3438b7405094SkyPlay</controlURL>\n"
+                + "                <eventSubURL>/444D5276-3247-4761-7465-3438b7405094SkyPlay</eventSubURL>\n"
+                + "            </service>\n" + "            <service>\n"
+                + "                <serviceType>urn:schemas-nds-com:service:SkyCM:2</serviceType>\n"
+                + "                <serviceId>urn:nds-com:serviceId:SkyCM</serviceId>\n"
+                + "                <SCPDURL>/player_cm.xml</SCPDURL>\n"
+                + "                <controlURL>/444D5276-3247-4761-7465-3438b7405094SkyCM</controlURL>\n"
+                + "                <eventSubURL>/444D5276-3247-4761-7465-3438b7405094SkyCM</eventSubURL>\n"
+                + "            </service>\n" + "            <service>\n"
+                + "                <serviceType>urn:schemas-nds-com:service:SkyRC:2</serviceType>\n"
+                + "                <serviceId>urn:nds-com:serviceId:SkyRC</serviceId>\n"
+                + "                <SCPDURL>/player_rc.xml</SCPDURL>\n"
+                + "                <controlURL>/444D5276-3247-4761-7465-3438b7405094SkyRC</controlURL>\n"
+                + "                <eventSubURL>/444D5276-3247-4761-7465-3438b7405094SkyRC</eventSubURL>\n"
+                + "            </service>\n" + "        </serviceList>\n" + "    </device>\n"
+                + "    <URLBase>http://192.168.178.23:49153/</URLBase>\n" + "</root>";
 
-        ServiceDescription serviceDescription = (ServiceDescription) xStream.fromXML(xml);
+        org.openhab.binding.skyq.internal.models.ServiceDescription serviceDescription = (org.openhab.binding.skyq.internal.models.ServiceDescription) xStream
+                .fromXML(xml);
 
-        assertEquals(serviceDescription.device.modelName,"Sky+HD");
-        assertEquals(serviceDescription.device.serviceList.get(0).serviceType,"urn:schemas-nds-com:service:SkyPlay:2");
+        assertEquals(serviceDescription.device.modelName, "Sky+HD");
+        assertEquals(serviceDescription.device.serviceList.get(0).serviceType, "urn:schemas-nds-com:service:SkyPlay:2");
     }
 
-    @XStreamAlias("root")
-    public static class ServiceDescription {
-        @XStreamAlias("device")
-        Device device;
-
-        @XStreamAlias("URLBase")
-        String urlBase;
-
+    @Test
+    public void testMessageFormat() {
+        final String template = "first value: {0}, second value: {1}, first value: {0}";
+        System.out.println(MessageFormat.format(template, "1", "2"));
     }
 
-    @XStreamAlias("device")
-    public static class Device {
+    @Test
+    public void testSOAPResponseXML() {
 
-        @XStreamAlias("deviceType")
-        String deviceType;
-
-        @XStreamAlias("modelName")
-        String modelName;
-
-        @XStreamAlias("serviceList")
-        private List<Service> serviceList;
-
+        XStream xStream = new XStream(new StaxDriver());
+        xStream.allowTypesByWildcard(new String[] { SkyQHandlerFactory.class.getPackageName() + ".**" });
+        xStream.setClassLoader(getClass().getClassLoader());
+        xStream.ignoreUnknownElements();
+        xStream.processAnnotations(MediaInfo.class);
+        String xml = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body>\n"
+                + "<u:GetMediaInfoResponse xmlns:u=\"nds-comSkyPlay:2\">\n" + "<NrTracks>0</NrTracks>\n"
+                + "<MediaDuration>00:00:00</MediaDuration>\n" + "<CurrentURI>xsi://7A</CurrentURI>\n"
+                + "<CurrentURIMetaData>NOT_IMPLEMENTED</CurrentURIMetaData>\n" + "<PlayMedium>NONE</PlayMedium>\n"
+                + "<RecordMedium>NOT_IMPLEMENTED</RecordMedium>\n" + "<WriteStatus>NOT_IMPLEMENTED</WriteStatus>\n"
+                + "</u:GetMediaInfoResponse>\n" + "</s:Body> </s:Envelope>";
+        System.out.println(xml);
+        MediaInfo mediaInfo = (MediaInfo) xStream.fromXML(xml);
+        System.out.println(mediaInfo.getCurrentURI());
     }
-
-    public static class ServiceList {
-        @XStreamImplicit
-        private @Nullable List<Service> services;
-    }
-
-    @XStreamAlias("service")
-    public static class Service {
-        @XStreamAlias("serviceType")
-        String serviceType;
-
-        @XStreamAlias("SCPDURL")
-        String SCPDURL;
-
-        @XStreamAlias("controlURL")
-        String controlURL;
-    }
-
 }
